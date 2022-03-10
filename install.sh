390cc6330fd1d204ab0a9146869bfd187ab0a610
lycoxz <84879541+lycoxz@users.noreply.github.com>
2022-03-10 20:38:19 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,26 +5,29 @@ r="\033[1;31m"
 b="\033[1;34m"
 w="\033[0m"
 mkdir $HOME/.config
-echo -e $b">"$w"Clash For Termux Non Root"
-echo -e $b">"$w" prepare for installing dependencies ..."
+echo -e $b"➤ "$w"Clash For Termux Non Root"
+echo -e $b"➤ "$w" prepare for installing dependencies ..."
 sleep 2
-echo -e $b">"$w" installing pacakge: "$g"clash"$w
+echo -e $b"➤ "$w" installing pacakge: "$g"clash"$w
 pkg install clash
-echo -e $b">"$w" installing pacakge: "$g"unzip"$w
+echo -e $b"➤ "$w" installing pacakge: "$g"unzip"$w
 pkg install unzip
-echo -e $b">"$w" installing pacakge: "$g"micro"$w
+echo -e $b"➤ "$w" installing pacakge: "$g"micro"$w
 pkg install micro
-echo -e $b">"$w" installing modules: "$g"wget"$w
+echo -e $b"➤ "$w" installing modules: "$g"wget"$w
 pkg install wget
-echo -e $b">"$w" installing modules: "$g"whiptail"$w
+echo -e $b"➤ "$w" installing modules: "$g"whiptail"$w
 pkg install whiptail
-echo -e $b">"$w" successfully installing dependencies"
-unzip .config.zip >&/dev/null
-sleep 4
-mv ClashForTermux $HOME/.config/ >&/dev/null
+echo -e $b"➤ "$w" successfully installing dependencies"
+cd $HOME/ClashForTermux
+sleep 2
+cp yacd-gh-pages.zip /data/data/com.termux/files/home/.config/clash
+sleep 2
+cp config.yaml /data/data/com.termux/files/home/.config/clash
+sleep 2
+unzip yacd-gh-pages.zip >&/dev/null
 sleep 2
 mv cft.sh $HOME >&/dev/null
 cd
 rm -r -f ClashForTermux >&/dev/null
 clear
-clash
