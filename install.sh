a3d10a004b21c2c0ead45857a3b1d2b130434304
lycoxz <84879541+lycoxz@users.noreply.github.com>
2022-03-10 20:40:56 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -25,8 +25,12 @@ cp yacd-gh-pages.zip /data/data/com.termux/files/home/.config/clash
 sleep 2
 cp config.yaml /data/data/com.termux/files/home/.config/clash
 sleep 2
+cd $HOME/.config/clash
 unzip yacd-gh-pages.zip >&/dev/null
+sleep 3
+rm -r -f yacd-gh-pages.zip >&/dev/null
 sleep 2
+cd $HOME/ClashForTermux
 mv cft.sh $HOME >&/dev/null
 cd
 rm -r -f ClashForTermux >&/dev/null
