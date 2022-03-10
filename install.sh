a9fec25f9487cd9bafd596e141e9823ce6d2dc23
lycoxz <84879541+lycoxz@users.noreply.github.com>
2022-03-11 02:42:40 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ g="\033[1;32m"
 r="\033[1;31m"
 b="\033[1;34m"
 w="\033[0m"
-mkdir $HOME/.config/clash >&/dev/null
+mkdir .config/clash
 echo -e $b"➤ "$w"Clash For Termux Non Root"
 echo -e $b"➤ "$w" prepare for installing dependencies ..."
 sleep 2
@@ -19,7 +19,7 @@ pkg install wget
 echo -e $b"➤ "$w" installing modules: "$g"whiptail"$w
 pkg install whiptail
 echo -e $b"➤ "$w" successfully installing dependencies"
-cd $HOME/ClashForTermux >&/dev/null
+cd ClashForTermux
 sleep 2
 echo -e $b"➤ "$w" coppying yacd-dashbord: "$g"yacd"$w
 cp yacd-gh-pages.zip /data/data/com.termux/files/home/.config/clash
@@ -27,12 +27,12 @@ sleep 2
 echo -e $b"➤ "$w" coppying config.yaml: "$g"config"$w
 cp config.yaml /data/data/com.termux/files/home/.config/clash
 sleep 2
-cd $HOME/.config/clash >&/dev/null
+cd .config/clash
 echo -e $b"➤ "$w" extracting yacd-dashbord: "$g"yacd"$w
-unzip yacd-gh-pages.zip >&/dev/null
-sleep 3
-rm -r -f yacd-gh-pages.zip >&/dev/null
+unzip yacd-gh-pages.zip
+sleep 2
+rm -r -f yacd-gh-pages.zip
 sleep 2
 cd
-rm -r -f ClashForTermux >&/dev/null
+rm -r -f ClashForTermux
 clear
