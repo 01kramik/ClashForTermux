0e45557254b548313c6941e7e53d18cd413702cb
lycoxz <84879541+lycoxz@users.noreply.github.com>
2022-03-11 03:24:15 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,36 +5,46 @@ r="\033[1;31m"
 b="\033[1;34m"
 w="\033[0m"
 mkdir $HOME/.config/clash
+echo ""
+
 echo -e $b"➤ "$w"Clash For Termux Non Root"
 echo ""
+
 echo -e $b"➤ "$w" prepare for installing dependencies ..."
 echo ""
-sleep 2
+
 echo -e $b"➤ "$w" installing pacakge: "$g"clash"$w
-echo ""
 pkg install clash
+echo ""
+
 echo -e $b"➤ "$w" installing pacakge: "$g"unzip"$w
-echo ""
 pkg install unzip
+echo ""
+
 echo -e $b"➤ "$w" installing pacakge: "$g"micro"$w
-echo ""
 pkg install micro
-echo -e $b"➤ "$w" installing modules: "$g"wget"$w
 echo ""
+
 cd $HOME/ClashForTermux
 sleep 2
+
 echo -e $b"➤ "$w" coppying yacd-dashbord: "$g"yacd"$w
-echo ""
 cp yacd-gh-pages.zip $HOME/.config/clash
-sleep 2
-echo -e $b"➤ "$w" coppying config.yaml: "$g"config"$w
 echo ""
-cp config.yaml $HOME/.config/clash
+
 sleep 2
+
+echo -e $b"➤ "$w" coppying config.yaml: "$g"config"$w
+cp config.yaml $HOME/.config/clash
+echo ""
+
+sleep 2
+
 cd $HOME/.config/clash
 echo -e $b"➤ "$w" extracting yacd-dashbord: "$g"yacd"$w
-echo ""
 unzip yacd-gh-pages.zip
+echo ""
+
 sleep 2
 rm -r -f yacd-gh-pages.zip
 sleep 2
