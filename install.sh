7e79921a0d2152f30e1ad78801f49af899923263
lycoxz <84879541+lycoxz@users.noreply.github.com>
2022-03-10 20:55:27 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ g="\033[1;32m"
 r="\033[1;31m"
 b="\033[1;34m"
 w="\033[0m"
-mkdir $HOME/.config
+mkdir $HOME/.config/clash
 echo -e $b"➤ "$w"Clash For Termux Non Root"
 echo -e $b"➤ "$w" prepare for installing dependencies ..."
 sleep 2
